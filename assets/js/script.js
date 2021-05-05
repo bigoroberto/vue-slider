02511153c833e58ea1e6179440eb5a3b77b86e09
bigoroberto <bigoroberto@gmail.com>
2021-05-05 18:33:49 +0200

    esercizio completato

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,24 @@ const root = new Vue({
 
     counterSlide:0,
     slider:[
-      'assets/img/image1.jpg',
-      'assets/img/image2.jpg',
-      'assets/img/image3.jpg',
-      'assets/img/image4.jpg'
+      'assets/img/image1.jpg', // 0
+      'assets/img/image2.jpg', // 1
+      'assets/img/image3.jpg', // 2
+      'assets/img/image4.jpg'  // 3
     ]
+    //length: 4
   },
   methods:{
     nextSlide(){
       this.counterSlide++;
-      if(this.counterSlide === slider.length) this.counterSlide= 0;
+      if(this.counterSlide === this.slider.length) this.counterSlide = 0;
+      console.log(this.slider.length);
     },
     prevSlide(){
       this.counterSlide--;
-      //quando il contatore imamgini è minore di 0 ritorna dall'ultima in su
-      if(this.counterSlide < 0) this.counterSlide = immagini.length;
+      //quando il contatore immagini è minore di 0 ritorna dall'ultima in su
+      if(this.counterSlide < 0) this.counterSlide = this.slider.length -1;
+      console.log(this.slider.length)
     }
   }
 
