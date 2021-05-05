fbe2634a083e40e4ff0e23f7bbef8112ab7bea48
bigoroberto <bigoroberto@gmail.com>
2021-05-05 17:43:17 +0200

    problema foto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const root = new Vue({
 
   el:'#root',
+  
 
   data:{
-    contatoreSlide = '',
+
+    counterSlide:0,
     slider:[
       'assets/img/image1.jpg',
       'assets/img/image2.jpg',
@@ -12,7 +14,15 @@ const root = new Vue({
     ]
   },
   methods:{
-
+    nextSlide(){
+      this.counterSlide++;
+      if(this.counterSlide === slider.length) this.counterSlide= 0;
+    },
+    prevSlide(){
+      this.counterSlide--;
+      //quando il contatore imamgini è minore di 0 ritorna dall'ultima in su
+      if(this.counterSlide < 0) this.counterSlide = immagini.length;
+    }
   }
 
 });
