a1270292f318d83eb94ef16eb4529ddf3693b448
bigoroberto <bigoroberto@gmail.com>
2021-05-05 17:18:28 +0200

    carousel

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const root = new Vue({
+
+  el:'#root',
+
+  data:{
+    contatoreSlide = '',
+    slider:[
+      'assets/img/image1.jpg',
+      'assets/img/image2.jpg',
+      'assets/img/image3.jpg',
+      'assets/img/image4.jpg'
+    ]
+  },
+  methods:{
+
+  }
+
+});
